6a6b9272cb56216cdc1536b793189bfa93d03c68
Ibrahim Farajzade <ibrahimfarajzade@gmail.com>
2025-04-20 18:53:49 +0300

    refactor sql queries

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -4,10 +4,10 @@ VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
 
 -- name: GetFeeds :many
-SELECT * from feeds;
+SELECT * FROM feeds;
 
 -- name: GetNextFeedsToFetch :many
-SELECT * from feeds
+SELECT * FROM feeds
 ORDER BY last_fetched_at ASC NULLS FIRST 
 LIMIT $1;
 
